603559ab74b5196294e05db1bc84be59f6e09255
wangzhi <wang.zhi@intellif.com>
2019-12-09 20:53:17 +0800

    添加redis产生手机验证码以及swagger支持

--- mall-spring-boot-mybatis/src/main/java/com/wz/mall/controller/PmsBrandController.java
+++ mall-spring-boot-mybatis/src/main/java/com/wz/mall/controller/PmsBrandController.java
@@ -4,15 +4,24 @@ import com.wz.mall.common.CommonPage;
 import com.wz.mall.common.CommonResult;
 import com.wz.mall.mbg.model.PmsBrand;
 import com.wz.mall.service.PmsBrandService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
+/**
+ * 品牌管理Controller
+ * Created by macro on 2019/4/19.
+ */
+@Api(tags = "PmsBrandController", description = "商品品牌管理")
+@Controller
+@RequestMapping("/brand")
 public class PmsBrandController {
 
     @Autowired
@@ -20,12 +29,14 @@ public class PmsBrandController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(PmsBrandController.class);
 
+    @ApiOperation("获取所有品牌列表")
     @RequestMapping(value = "listAll", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<List<PmsBrand>> getBrandList() {
         return CommonResult.success(demoService.listAllBrand());
     }
 
+    @ApiOperation("添加品牌")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     @ResponseBody
     public CommonResult createBrand(@RequestBody PmsBrand pmsBrand) {
@@ -41,6 +52,7 @@ public class PmsBrandController {
         return commonResult;
     }
 
+    @ApiOperation("更新指定id品牌信息")
     @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
     @ResponseBody
     public CommonResult updateBrand(@PathVariable("id") Long id, @RequestBody PmsBrand pmsBrandDto, BindingResult result) {
@@ -56,6 +68,7 @@ public class PmsBrandController {
         return commonResult;
     }
 
+    @ApiOperation("删除指定id的品牌")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult deleteBrand(@PathVariable("id") Long id) {
@@ -69,6 +82,7 @@ public class PmsBrandController {
         }
     }
 
+    @ApiOperation("分页查询品牌列表")
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<CommonPage<PmsBrand>> listBrand(@RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
@@ -77,6 +91,7 @@ public class PmsBrandController {
         return CommonResult.success(CommonPage.restPage(brandList));
     }
 
+    @ApiOperation("获取指定id的品牌详情")
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public CommonResult<PmsBrand> brand(@PathVariable("id") Long id) {
--- mall-spring-boot-mybatis/src/main/java/com/wz/mall/mbg/mapper/PmsBrandMapper.java
+++ mall-spring-boot-mybatis/src/main/java/com/wz/mall/mbg/mapper/PmsBrandMapper.java
@@ -4,9 +4,7 @@ import com.wz.mall.mbg.model.PmsBrand;
 import com.wz.mall.mbg.model.PmsBrandExample;
 import java.util.List;
 import org.apache.ibatis.annotations.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PmsBrandMapper {
     int countByExample(PmsBrandExample example);
 
